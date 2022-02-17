2997ebba4611e5c05a458406dc265a603a89a551
Dan Amar <danamar2001@gmail.com>
2022-02-17 17:00:37 -0500

    began bookmark controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,6 @@
 class BookmarksController < ApplicationController
+
+  def new
+    @bookmark =
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,4 +8,29 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list, notice: 'List was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, notice: 'List has been deleted.'
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
